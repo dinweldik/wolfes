146bfece456b85e2f28936c6851be8404186deb1
wael <wael@dinweldik.com>
2024-02-09 19:41:32 +0100

    style success page

--- src/routes/anmeldung/success/+page.svelte
+++ src/routes/anmeldung/success/+page.svelte
@@ -6,12 +6,23 @@
 
 <section class="content">
 	<!-- 	<SuccessAnmeldung data={data.record} /> -->
-	<p class="description">
-		Vielen Dank für Ihre Anmeldung. Wir haben Ihre Anmeldung erhalten und werden uns in Kürze bei
-		Ihnen melden.
-	</p>
-	<p class="description">Mit freundlichen Grüßen</p>
-	<p class="description">Ihr SV Fußballwölfe Team</p>
-	<p class="description">Waldgasse 1, 3001 Mauerbach</p>
-	<p class="description"></p>
+	<div class="center">
+		<h1>Vielen Dank für Ihre Anmeldung.</h1>
+
+		<h3>Wir haben Ihre Anmeldung erhalten und werden uns bei Ihnen melden.</h3>
+		<div class="container">
+			<p>Mit freundlichen Grüßen,</p>
+			<p>Ihr SV Fußballwölfe Team</p>
+		</div>
+	</div>
 </section>
+
+<style>
+	h1,
+	h3 {
+		text-align: center;
+	}
+	.container {
+		margin: var(--size-6);
+	}
+</style>

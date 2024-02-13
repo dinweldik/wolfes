8330eb6a952f1999daa0d4934602e6459f479d25
wael <wael@dinweldik.com>
2024-02-13 22:01:58 +0100

    fix checkbox on fucking iphone

--- src/lib/components/Checkbox.svelte
+++ src/lib/components/Checkbox.svelte
@@ -17,9 +17,12 @@
 		border-radius: var(--size-2);
 		-webkit-transition: all ease-out 0.2s;
 		transition: all ease-out 0.2s;
-		font: var(--text-xs);
 		margin-block: var(--size-3);
 		padding: var(--size-3);
+	}
+
+	span {
+		font: var(--text-xs);
 	}
 	input {
 		margin-right: var(--size-2);
